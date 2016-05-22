0ce722178030493fe49c8e85cf047061a5014931
vsoch <vsochat@stanford.edu>
2016-05-22 15:38:27 -0700

    neuropowertools is missing __init__.py

--- neuropower/manage.py
+++ neuropower/manage.py
@@ -2,6 +2,8 @@
 import os
 import sys
 
+#root_path = os.path.abspath(os.path.split(__file__)[0])
+#sys.path.insert(0, os.path.join(root_path, 'neuropowertools'))
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "neuropowertools.settings")
 
